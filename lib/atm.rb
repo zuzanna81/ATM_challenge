4f97606181a9042199fa2393236029c010fa6892
Zuzanna <zuzanna.bienkowska@gmail.com>
2017-10-31 21:48:08 +0100

    Refactor and add private methods

--- lib/atm.rb
+++ lib/atm.rb
@@ -5,20 +5,25 @@ class Atm
     @funds = 1000
   end
 
-  def withdraw(amount)
-    @funds -= amount
-  end
-
   def withdraw(amount, account)
     case
-    when amount > account.balance
+    when insufficient_funds_in_account?(amount, account)
       return
     else
-      @funds -= amount
-      account.balance = account.balance - amount
-      { status: true, message: 'success', date: Date.today, amount: amount }
+      perform_transaction(amount, account)
     end
 
-    end
+end
 
+private
+
+  def insufficient_funds_in_account?(amount, account)
+    amount > account.balance
+  end
+
+  def perform_transaction(amount, account)
+    @funds -= amount
+    account.balance = account.balance - amount
+    { status: true, message: 'success', date: Date.today, amount: amount }
+  end
 end
