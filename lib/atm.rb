1daed51963f5e4f4e0d16984b1094e40d81b34ad
Zuzanna <zuzanna.bienkowska@gmail.com>
2017-11-01 20:38:04 +0100

    Add bills and while method

--- lib/atm.rb
+++ lib/atm.rb
@@ -40,7 +40,19 @@ private
   def perform_transaction(amount, pin_code, account)
     @funds -= amount
     account.balance = account.balance - amount
-    { status: true, message: 'success', date: Date.today, amount: amount }
+    { status: true, message: 'success', date: Date.today, amount: amount, bills: add_bills(amount) }
+  end
+
+  def add_bills(amount)
+    denominations = [20, 10, 5]
+    bills = []
+    denominations.each do |bill|
+      while amount - bill >= 0
+        amount -= bill
+        bills << bill
+      end
+    end
+    bills
   end
 
   def card_expired?(exp_date)
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -19,8 +19,8 @@ describe Atm do
   end
 
   it 'allow withdraw if account has enough balance' do
-    expected_output = { status: true, message: 'success', date: Date.today, amount:50}
-    expect(subject.withdraw(50, '1234', account)).to eq expected_output
+    expected_output = { status: true, message: 'success', date: Date.today, amount:45, bills: [20, 20, 5] }
+    expect(subject.withdraw(45, '1234', account)).to eq expected_output
   end
 
   it 'reject withdraw if ATM has insufficient funds' do
