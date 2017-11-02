4fdb04dce467fe98bf8c4d5b4cf4f1f31f61f6d3
Zuzanna <zuzanna.bienkowska@gmail.com>
2017-11-02 14:00:38 +0100

    Rephrase some specs

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -11,25 +11,25 @@ describe Account do
     expect(pin_length).to eq 4
   end
 
-  it 'expect to have an expiry date on initialize' do
+  it 'account is expected to have an expiry date on initialize' do
     exp_date = Date.today.next_year(5).strftime("%m/%y")
     expect(exp_date).to eq '11/22'
   end
 
-  it 'expect to have :active status on initialize' do
+  it 'account is expected to have :active status on initialize' do
     expect(subject.account_status).to eq :active
   end
 
-  it 'deactivate account using Instance method' do
+  it 'deactivates account using Instance method' do
     subject.deactivate
     expect(subject.account_status).to eq :deactivated
   end
 
-  it 'expect to have an owner' do
+  it 'account is expected to have an owner' do
     expect(subject.owner).to eq person
   end
 
-  it 'expect to raise error if no owner is set' do
+  it 'account is expected to raise error if no owner is set' do
     expect { described_class.new }.to raise_error 'An account owner is required'
   end
 
