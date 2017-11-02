f3ffabb3aa7a6d0595028d6c78e1763953a58ca4
Zuzanna <zuzanna.bienkowska@gmail.com>
2017-11-02 17:13:35 +0100

    Add account attribute to attr_accessor list

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,6 @@
 class Person
 
-attr_accessor :name, :cash
+attr_accessor :name, :cash, :account
 
 def initialize(attrs = {})
   @name = set_name(attrs[:name])
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -12,8 +12,12 @@ describe Person do
     expect { described_class.new }. to raise_error 'A name is required'
   end
 
-it 'is expected to have a :cash attribute with value of 0 on initialize' do
-  expect(subject.cash).to eq 0
-end
+  it 'is expected to have a :cash attribute with value of 0 on initialize' do
+    expect(subject.cash).to eq 0
+  end
+
+  it 'is expected to have a :account attribute' do
+    expect(subject.account).to be nil
+  end
 
 end
