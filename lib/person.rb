6e55409ee0f416ac647bb4a0348b6785833ab5cc
Zuzanna <zuzanna.bienkowska@gmail.com>
2017-11-02 17:06:11 +0100

    Add error message when name is missing

--- lib/person.rb
+++ lib/person.rb
@@ -13,4 +13,8 @@ def set_name(name)
   #name.nil? ? missing_name : name
 end
 
+def missing_name
+  raise 'A name is required'
+end
+
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -7,4 +7,10 @@ describe Person do
   it 'is expected to have a :name on initialize' do
     expect(subject.name).not_to be nil
   end
+
+  it 'is expected to raise error if no name is set' do
+    expect { described_class.new }. to raise_error 'A name is required'
+  end
+
+
 end
